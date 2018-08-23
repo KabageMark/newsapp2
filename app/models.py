1572622cfd261ddc467d1cf3e893d41b53d7cf5a
kabagemark <kabagemark@gmail.com>
2018-08-23 12:22:26 +0300

    Articles class added to models

--- app/models.py
+++ app/models.py
@@ -9,3 +9,16 @@ class NEWS:
         self.description = description
         self.url = url
         self.country = country
+
+class ARTICLES:
+    '''
+    NEWS class to define NEWS Objects
+    '''
+
+    def __init__(self,id,author,description,Url,urlToImage,publishedAt):
+        self.id =id
+        self.author = author
+        self.description = description
+        self.Url = Url
+        self.urlToImage = urlToImage 
+        self.publishedAt = publishedAt         
