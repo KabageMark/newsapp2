e9b54297b592e130f278880bcc2b9d6af486d1f6
kabagemark <kabagemark@gmail.com>
2018-08-23 12:19:22 +0300

    class news added inside the models

---
+++ app/models.py
@@ -0,0 +1,11 @@
+class NEWS:
+    '''
+    NEWS class to define NEWS Objects
+    '''
+
+    def __init__(self,id,name,description,url,country):
+        self.id =id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.country = country
