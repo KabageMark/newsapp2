29a83ffacb9dd62dae63843b87ef6d45f4d38976
kabagemark <kabagemark@gmail.com>
2018-08-24 11:19:56 +0300

    import error resolved

--- manage.py
+++ manage.py
@@ -1,6 +1,15 @@
-from flask import Flask
-app = Flask(__name__)
+from app import create_app
+from flask_script import Manager,Server
 
+# Creating app instance
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+
+manager = Manager(app)
+manager.add_command('server',Server)
 if __name__ == '__main__':
-    app.run(debug = True)
+    manager.run()
 
