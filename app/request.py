c40e0cd2c9537dbfe09acab45bc83f6196803484
kabagemark <kabagemark@gmail.com>
2018-08-24 11:28:12 +0300

    process source function added

--- app/request.py
+++ app/request.py
@@ -2,7 +2,7 @@ from app import app
 import urllib.request,json
 from .models import NEWS,ARTICLES
 News = NEWS
-Articles =ARTICLES
+Articles = ARTICLES
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
@@ -26,3 +26,26 @@ def get_source(category):
             source_results_list = get_news_response['sources']
             source_results = process_source(source_results_list)
     return source_results
+
+def process_source(source_list):
+    '''
+    Function  that processes the news result and transform them to a list of Objects
+
+        Args:
+    news_list: A list of dictionaries that contain movie details
+
+        Returns :
+    news_results: A list of news objects
+    '''
+    source_results = []
+    for source_item in source_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        Url = source_item.get('Url')
+        description = source_item.get('description')
+        country = source_item.get('country')
+
+        source_object = News(id,name,country,description,Url)
+        source_results.append(source_object)
+    # print(source_results)
+    return source_results    
