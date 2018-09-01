4007c8ea3ae8c253d358ad4a844026116ff5b4fe
kabagemark <kabagemark@gmail.com>
2018-09-01 11:35:06 +0300

    tests added to test folder

---
+++ tests/test_news.py
@@ -0,0 +1,44 @@
+from .models import NEWS,ARTICLES
+import unittest
+class testNews(unittest.TestCase):
+    def setUp(self):
+            '''
+            Set up method to run before each test cases.
+            '''
+            self.new_news = NEWS(1,"BBC","Michael Cohen prosecuted","http://www.aljazeera.com","us") 
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_news.id,1)
+        self.assertEqual(self.new_news.description,"BBC")
+        self.assertEqual(self.new_news.url,"Michael Cohen prosecuted")
+        self.assertEqual(self.new_news.country,"us")
+
+
+    # def test_save_Account(self):
+    #     '''
+    #     test_save_contact test case to test if the contact object is saved into
+    #     the contact list
+    #     '''
+    #     self.new_account.saveaccount() # saving the new contact
+    #     self.assertEqual(len(Account.account_list),1)
+
+    # def tearDown(self):
+    #     '''
+    #     tearDown method that does clean up after each test case has run.
+    #     '''
+    #     Account.account_list = []    
+    
+    # def test_save_multiple_Account(self):
+    #     '''
+    #     test_save_contact test case to test if the contact object is saved into
+    #     the contact list
+    #     '''
+    #     self.new_account.saveaccount() 
+    #     test_account = Account("Test","user","test@user.com") 
+    #     test_account.saveaccount()
+    #     self.assertEqual(len(Account.account_list),2)
